f91e660c479af8464ecb8dc10576f3e0255e23da
kaiii299 <67326492+kaiii299@users.noreply.github.com>
2025-03-01 05:38:56 +0800

    Added cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,41 @@
-import { DataTableDemo } from "@/components/tableView";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div className="w-full">
-      <div className="grid grid-cols-3">
-        <Card>
-          
+      <div className="grid grid-cols-3 gap-5">
+        <Card >
+          <CardHeader>
+            <CardTitle></CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+          <CardContent className="flex justify-center items-center">
+            <h2 className="text-4xl font-bold">100%</h2>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+          </CardFooter>
+        </Card>
+        <Card >
+          <CardHeader>
+            <CardTitle></CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+          <CardContent className="flex justify-center items-center">
+            <h2 className="text-4xl font-bold">100%</h2>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+          </CardFooter>
+        </Card>
+        <Card >
+          <CardHeader>
+            <CardTitle></CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+          <CardContent className="flex justify-center items-center">
+            <h2 className="text-4xl font-bold">100%</h2>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+          </CardFooter>
         </Card>
       </div>
     </div>
