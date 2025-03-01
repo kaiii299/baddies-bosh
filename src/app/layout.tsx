eec27479cb40589dd447f589d4e352bf46378154
Kai Yuan <shodusbus@gmail.com>
2025-03-02 05:14:54 +0800

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { NavBar } from "@/components/NavBar";
+// import { NavBar } from "@/components/NavBar";
 import { SidebarComponent } from "@/components/sidebarComponent";
 import { Toaster } from "@/components/ui/sonner";
 
