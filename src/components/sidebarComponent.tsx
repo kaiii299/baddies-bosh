5f9213d192aaa171a3d323b5b2fe4f070798aade
kaiii299 <67326492+kaiii299@users.noreply.github.com>
2025-03-02 01:02:47 +0800

    FIX route

--- src/components/sidebarComponent.tsx
+++ src/components/sidebarComponent.tsx
@@ -1,67 +1,75 @@
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-} from "@/components/ui/sidebar"
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
 
-import { Calendar, File, Home, Settings, Settings2, Table2, UserRound } from "lucide-react"
+import {
+  Calendar,
+  File,
+  Home,
+  Settings,
+  Settings2,
+  Table2,
+  UserRound,
+} from "lucide-react";
 
 // Menu items.
 const items = [
-    {
-        title: "Home",
-        url: "/",
-        icon: Home,
-    },
-    {
-        title: "Calendar",
-        url: "/calendar",
-        icon: Calendar,
-    },
-    {
-        title: "Settings",
-        url: "/",
-        icon: Settings2,
-    },
-    {
-        title: "Table",
-        url: "/tools",
-        icon: Table2,
-    },
-    {
-        title: "Profile",
-        url: "#",
-        icon: UserRound,
-    },
-]
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Calendar",
+    url: "/calendar",
+    icon: Calendar,
+  },
+  {
+    title: "Table",
+    url: "/tools",
+    icon: Table2,
+  },
+  {
+    title: "Settings",
+    url: "/",
+    icon: Settings2,
+  },
+  {
+    title: "Profile",
+    url: "#",
+    icon: UserRound,
+  },
+];
 
 export function SidebarComponent() {
-    return (
-        <Sidebar collapsible="icon">
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupLabel>Application</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <a href={item.url}>
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </a>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-        </Sidebar>
-    )
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
 }
