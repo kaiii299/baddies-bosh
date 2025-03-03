6f32f02838b3eba5c173b3a7ec62b63b0a3b5194
kaiii299 <67326492+kaiii299@users.noreply.github.com>
2025-03-03 15:56:32 +0800

    updated to 23 tools

--- src/components/Home/dashboard.tsx
+++ src/components/Home/dashboard.tsx
@@ -77,7 +77,7 @@ export default function Dashboard({ tools }: props) {
     {
       title: "Need Calibration",
       description: "Within next 3 months",
-      value: 7,
+      value: 23,
     },
   ];
 
